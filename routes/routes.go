5b0ce665332d002bc5805decc519803d92b4d821
markdormeus <serran.mark.ms@gmail.com>
2024-10-23 20:23:57 -0400

    Update routes.go
    Implemented proper routing after file structure change

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"todolist-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine) {
+	router.GET("/tasks", controllers.GetTasks)
+	router.GET("/tasks/:id", controllers.GetTaskByID)
+	router.PUT("tasks/:id", controllers.UpdateTask)
+	router.POST("/tasks", controllers.CreateTask)
+	router.DELETE("/tasks/:id", controllers.DeleteTask)
+
+	router.Run("localhost:8080")
+}
